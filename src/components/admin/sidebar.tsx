83bf115b641b779dcd65481d39217e12c9d25138
Gaku Ishikawa <gaku.jp.301@gmail.com>
2025-06-23 21:18:52 +0900

    create account

--- src/components/admin/sidebar.tsx
+++ src/components/admin/sidebar.tsx
@@ -1,6 +1,11 @@
 'use client';
 
-import { ThemeToggleButton } from '@/components/theme-toggle-button';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
 import {
   Sidebar,
   SidebarContent,
@@ -10,8 +15,17 @@ import {
   SidebarMenuButton,
   SidebarMenuItem,
 } from '@/components/ui/sidebar';
-import { FileText, Home, Megaphone } from 'lucide-react';
+import {
+  ChevronUp,
+  DollarSign,
+  FileText,
+  Home,
+  LogOut,
+  Megaphone,
+  User2,
+} from 'lucide-react';
 import Link from 'next/link';
+import { ThemeToggleButton } from '../theme-toggle-button';
 
 export default function AdminSidebar() {
   const menus = [
@@ -51,37 +65,35 @@ export default function AdminSidebar() {
             </SidebarMenuItem>
           ))}
         </SidebarMenu>
-
-        {/* <SidebarGroup>
-          <SidebarGroupLabel>Forms</SidebarGroupLabel>
-          <SidebarGroupContent>
-            <SidebarMenu>
-              <SidebarMenuItem>
-                <SidebarMenuButton asChild>
-                  <Link href="/admin/forms" className="sidebar-item">
-                    <ListOrdered />
-                    <span>Lists</span>
-                  </Link>
-                </SidebarMenuButton>
-              </SidebarMenuItem>
-              <SidebarMenuItem>
-                <SidebarMenuButton asChild>
-                  <Link href="/admin/forms/create" className="sidebar-item">
-                    Create
-                  </Link>
-                </SidebarMenuButton>
-              </SidebarMenuItem>
-            </SidebarMenu>
-          </SidebarGroupContent>
-        </SidebarGroup> */}
       </SidebarContent>
       <SidebarFooter>
         <SidebarMenu>
-          <SidebarMenuItem className="justify-between flex items-center p-2">
-            <span>User Name</span>
-            <SidebarMenuButton asChild>
-              <ThemeToggleButton />
-            </SidebarMenuButton>
+          <SidebarMenuItem>
+            <DropdownMenu>
+              <div className="flex items-center">
+                <DropdownMenuTrigger asChild className="">
+                  <SidebarMenuButton className="border py-4 dark:bg-input/30 bg-background ">
+                    <User2 /> Username
+                    <ChevronUp className="ml-auto" />
+                  </SidebarMenuButton>
+                </DropdownMenuTrigger>
+                <ThemeToggleButton />
+              </div>
+              <DropdownMenuContent side="top" className="">
+                <DropdownMenuItem>
+                  <User2 />
+                  <span>Account</span>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  <DollarSign />
+                  <span>Billing</span>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  <LogOut />
+                  <span>Sign out</span>
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
           </SidebarMenuItem>
         </SidebarMenu>
       </SidebarFooter>
